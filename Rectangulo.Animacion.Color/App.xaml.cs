3ea5e9769eb4cb96d2b07ee6de1b8ab228f45cca
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-09-24 13:43:20 -0500

    Commit

---
+++ Rectangulo.Animacion.Color/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Rectangulo.Animacion.Color
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
